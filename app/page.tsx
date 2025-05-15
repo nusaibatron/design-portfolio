8ccc2a20dafbfe49de26affa841cfb5f216c4276
nusaibatron <nusaiba@berkeley.edu>
2025-05-15 14:58:50 -0400

    Haha

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-white text-black dark:bg-black dark:text-white px-4">
-      <h1 className="text-4xl font-bold mb-4">Hi, Im Nusaiba</h1>
-      <p className="text-lg">This is my styled site ✨</p>
+      <h1 className="text-4xl font-bold mb-4">Hey im Nusaiba</h1>
+      <p className="text-lg">This is where ill put things to show you haha✨</p>
     </main>
   );
 }
