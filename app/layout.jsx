2ff72984bc052bc293f309888dbc3726f736cf89
nusaibatron <nusaiba@berkeley.edu>
2025-05-16 11:39:06 -0400

    Nav bar adjust

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,42 +19,55 @@ export default function RootLayout({ children }) {
         backgroundColor: "#f9f9fb",
         color: "#0f0f0f",
       }}>
-        <header style={{
-          backgroundColor: "#0a1a30",
-          display: "flex",
-          alignItems: "center",
-          padding: "1rem 2rem",
-          boxShadow: "0 1px 4px rgba(0,0,0,0.05)",
-          position: "relative"
-        }}>
-          {/* Home icon on non-home pages */}
-          {!isHome && (
-            <Link href="/" style={{
-              position: "absolute",
-              left: "2rem",
-              color: "#fff",
-              fontSize: "1.5rem",
-              textDecoration: "none"
-            }}>
-              🏠
-            </Link>
-          )}
+<header style={{
+  backgroundColor: "#0a1a30",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "0 1.5rem", // Remove vertical padding
+  boxShadow: "0 1px 4px rgba(0,0,0,0.05)",
+  position: "relative",
+  height: "64px", // Fixed height for navbar
+  minHeight: "64px"
+}}>
+  {/* Left: Home icon */}
+  <div style={{ width: "25%", display: "flex", justifyContent: "flex-start", height: "100%", alignItems: "center" }}>
+    {!isHome && (
+      <Link href="/" style={{
+        color: "#fff",
+        fontSize: "1.5rem",
+        textDecoration: "none",
+        height: "100%",
+        display: "flex",
+        alignItems: "center"
+      }}>
+        🏠
+      </Link>
+    )}
+  </div>
 
-          {/* Centered nav */}
-          <nav style={{
-            margin: "0 auto",
-            display: "flex",
-            gap: "5rem",
-            fontSize: "1.1rem",
-            fontWeight: 600,
-            color: "#fff",
-            textAlign: "center"
-          }}>
-            <Link href="/projects" style={{ textDecoration: "none", color: "white" }}>Projects</Link>
-            <Link href="/about" style={{ textDecoration: "none", color: "white" }}>About Me</Link>
-            <Link href="/art" style={{ textDecoration: "none", color: "white" }}>Art</Link>
-          </nav>
-        </header>
+  {/* Center: Nav */}
+  <nav style={{
+    width: "33.33%", // Middle 1/3 of the page
+    display: "flex",
+    justifyContent: "space-between", // Evenly distribute links
+    fontSize: "1.1rem",
+    fontWeight: 600,
+    color: "#fff",
+    textAlign: "center",
+    height: "100%",
+    alignItems: "center"
+  }}>
+    <Link href="/projects" style={{ textDecoration: "none", color: "white", height: "100%", display: "flex", alignItems: "center" }}>Projects</Link>
+    <Link href="/about" style={{ textDecoration: "none", color: "white", height: "100%", display: "flex", alignItems: "center" }}>About Me</Link>
+    <Link href="/art" style={{ textDecoration: "none", color: "white", height: "100%", display: "flex", alignItems: "center" }}>Art</Link>
+  </nav>
+
+  {/* Right: placeholder to balance layout */}
+  <div style={{ width: "25%", height: "100%" }} />
+</header>
+
+
         {children}
       </body>
     </html>
